2f2ec16375d148a227d360feb06b39280f6cd54a
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-08-13 14:15:12 +0200

    one task left

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // task 1
-
 // let user;
 // let userAge = Number(prompt("How old are you?"));
-
 // if (userAge < 2) {
 //   user = "baby";
 // } else if (userAge >= 12 && userAge < 18) {
@@ -15,7 +13,6 @@
 
 // task 2
 // let number = Number(prompt("Enter number from 0 to 9"));
-
 // switch (number) {
 //     case 0:
 //         console.log("!");
@@ -51,7 +48,6 @@
 
 // task 3
 // let number = Number(prompt("Enter 3 digit number"));
-
 // if (number[0] === number[1]) {
 //     console.log("Yes");
 // } else if (number[1] === number[2]) {
@@ -64,9 +60,51 @@
 
 // task 4
 // let year = Number(prompt("Enter year"));
-
 // if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
 //   console.log("Leap year");
 // } else {
 //   console.log("Not leap year");
 // }
+
+// task 5
+// let num = Number(prompt("Enter 5 digit number"));
+// if (num[0] === num[4] && num[1] === num[3]) {
+//     console.log("The number is palindrome");
+// } else {
+//     console.log("The number is not palindrome");
+// }
+
+// task 6
+// let numStart = Number(prompt("Enter a starting number"));
+// let numEnd = Number(prompt("Enter an ending number"));
+// let sum = 0;
+// for (let i = numStart; i <= numEnd; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+// task 7
+// let numOne = Number(prompt("Enter first number"));
+// let numTwo = Number(prompt("Enter second number"));
+// function gcd(numOne, numTwo) {
+//     if (numTwo === 0) {
+//         return numOne;
+//     }
+//     return gcd(numTwo, numOne % numTwo);
+// }
+// console.log(gcd(numOne, numTwo));
+
+// task 8
+// let num = Number(prompt("Enter number"));
+// for (let i = 1; i <= number; i++) {
+//     if (number % i === 0) {
+//         console.log(i);
+//     }
+// }
+
+// task 9
+// let num = prompt("Enter number");
+// console.log(num.length());
+
+// task 10
+//?????
