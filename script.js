b2e2ee0101e75f39a536adbbead236da984352e2
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-08-13 13:50:43 +0200

    tasks 1-4

---
+++ script.js
@@ -0,0 +1,72 @@
+// task 1
+
+// let user;
+// let userAge = Number(prompt("How old are you?"));
+
+// if (userAge < 2) {
+//   user = "baby";
+// } else if (userAge >= 12 && userAge < 18) {
+//   user = "teenager";
+// } else if (userAge >= 18 && userAge < 60) {
+//   user = "adult";
+// } else {
+//   user = "senior";
+// }
+
+// task 2
+// let number = Number(prompt("Enter number from 0 to 9"));
+
+// switch (number) {
+//     case 0:
+//         console.log("!");
+//         break;
+//     case 1:
+//         console.log("@");
+//         break;
+//     case 2:
+//         console.log("#");
+//         break;
+//     case 3:
+//         console.log("$");
+//         break;
+//     case 4:
+//         console.log("%");
+//         break;
+//     case 5:
+//         console.log("^");
+//         break;
+//     case 6:
+//         console.log("&");
+//         break;
+//     case 7:
+//         console.log("*");
+//         break;
+//     case 8:
+//         console.log("(");
+//         break;
+//     case 9:
+//         console.log(")");
+//         break;
+// }
+
+// task 3
+// let number = Number(prompt("Enter 3 digit number"));
+
+// if (number[0] === number[1]) {
+//     console.log("Yes");
+// } else if (number[1] === number[2]) {
+//     console.log("Yes");
+// } else if (number[0] === number[2]) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+// task 4
+// let year = Number(prompt("Enter year"));
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//   console.log("Leap year");
+// } else {
+//   console.log("Not leap year");
+// }
